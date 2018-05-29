ae0bd6d6331ae44e563ebe8ba350749d447ddd3d
bailey-mcb <38860866+bailey-mcb@users.noreply.github.com>
2018-05-29 10:08:28 -0400

    Update MapScreen.java

--- MyCity/src/MyCity/MapScreen.java
+++ MyCity/src/MyCity/MapScreen.java
@@ -1,196 +1,262 @@
 package MyCity;
 
-import java.awt.Color;
-import java.awt.Font;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
+
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
-public class Results {
-//variables that will show up on results screen
-static String city1;
-static String city2;
-static String city3;
-static String city4;
-static String city5;
-static String city6;
-static String city7;
-static String city8;
-static String city9;
-static String city10;
 
-//variables that will be used for map
-static String city1S;
-static String city2S;
-static String city3S;
-static String city4S;
-static String city5S;
-static String city6S;
-static String city7S;
-static String city8S;
-static String city9S;
-static String city10S;
-
-	public static void main(String [] args) {
-		run();
+public class MapScreen {
+	static Font font = new Font("Verdana", Font.PLAIN, 20);
+	static String[] opt = {"Chicago+IL", "Hampton+VA", "Atlanta+GA", "Boston+MA", "Brooklyn+NY", "Seattle+WA", "Houston+TX", "Marathon+FL", "Ann Arbor+MI", "Los Angeles+CA"};
+	static String place = "";
+	public static void main(String[] args) {	
+		run(opt);
 	}
-	
-	public static void run() {
-		//JFrame for results screen
-		JFrame results = new JFrame("Results");
-		results.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		results.setLayout(null);
-		results.setExtendedState(JFrame.MAXIMIZED_BOTH);
-		results.getContentPane().setBackground(new Color(13,229,175));
+
+	public static void run(String[] opt) {
+
+		// Build the Browser Frame
+		JFXPanel jfxPanel = new JFXPanel();
 		
-		//title for results page
-		JLabel resultTitle = new JLabel("Your Results");
-		resultTitle.setBounds(750, 50, 900, 100);
-		resultTitle.setFont(new Font("Verdana", Font.BOLD, 72));
-		results.add(resultTitle);
-		
-		//font for top ten
-		Font topFont = new Font("Verdana", Font.PLAIN, 30);
-		
-		//JLabel to list results
-		JLabel YourTopTen = new JLabel("Top ten cities for you: ");
-		YourTopTen.setFont(topFont);
-		YourTopTen.setBounds(825, 150, 400, 75);
-		results.add(YourTopTen);
-		
-		//JLabel for first pick
-		city1 = "Houston, Texas";
-		city1S = "Houston+TX";
-		JLabel num1 = new JLabel ("1. " + city1);
-		num1.setFont(topFont);
-		num1.setBounds(825, 200, 600, 75);
-		results.add(num1);
-		
-		//JLabel for second pick
-		city2 = "Jacksonville, Florida";
-		city2S = "Jacksonville+FL";
-		JLabel num2 = new JLabel ("2. " + city2);
-		num2.setFont(topFont);
-		num2.setBounds(825, 250, 600, 75);
-		results.add(num2);
-		
-		//JLabel for third pick
-		city3 = "Daytona, Florida";
-		city3S = "Daytona+FL";
-		JLabel num3 = new JLabel ("3. " + city3);
-		num3.setFont(topFont);
-		num3.setBounds(825, 300, 600, 75);
-		results.add(num3);
-				
-		//JLabel for fourth pick
-		city4 = "Orlando, Florida";
-		city4S = "Orlando+FL";
-		JLabel num4 = new JLabel ("4. " + city4);
-		num4.setFont(topFont);
-		num4.setBounds(825, 350, 600, 75);
-		results.add(num4);		
-				
-		//JLabel for fifth pick
-		city5 = "Miami, Florida";
-		city5S = "Miami+FL";
-		JLabel num5 = new JLabel ("5. " + city5);
-		num5.setFont(topFont);
-		num5.setBounds(825, 400, 600, 75);
-		results.add(num5);	
-		
-		//JLabel for sixth pick
-		city6 = "West Palm Beach, Florida";
-		city6S = "West Palm Beach+FL";
-		JLabel num6 = new JLabel ("6. " + city6);
-		num6.setFont(topFont);
-		num6.setBounds(825, 450, 600, 75);
-		results.add(num6);
-		
-		//JLabel for seventh pick
-		city7 = "Tampa, Florida";
-		city7S = "Tampa+FL";
-		JLabel num7 = new JLabel ("7. " + city7);
-		num7.setFont(topFont);
-		num7.setBounds(825, 500, 600, 75);
-		results.add(num7);
-		
-		//JLabel for eighth pick
-		city8 = "Daytona, Florida";
-		city8S = "Daytona+FL";
-		JLabel num8 = new JLabel ("8. " + city8);
-		num8.setFont(topFont);
-		num8.setBounds(825, 550, 600, 75);
-		results.add(num8);
-		
-		//JLabel for ninth pick
-		city9 = "Sarasota, Florida";
-		city9S = "Sarasota+FL";
-		JLabel num9 = new JLabel ("9. " + city9);
-		num9.setFont(topFont);
-		num9.setBounds(825, 600, 600, 75);
-		results.add(num9);
-		
-		//JLabel for tenth pick
-		city10 = "Gainesville, Florida";
-		city10S = "Gainesville+FL";
-		JLabel num10 = new JLabel ("10. " + city10);
-		num10.setFont(topFont);
-		num10.setBounds(825, 650, 600, 75);
-		results.add(num10);		
-				
-		//button to view results on map screen
-		JButton viewMap = new JButton ("Cities on Map");
-		viewMap.setFont(new Font("Verdana", Font.BOLD, 40));
-		viewMap.setBackground(new Color(182,239,225));
-		viewMap.setBounds(550, 750, 400, 75);
-		viewMap.setBorderPainted(false);
-		results.add(viewMap);
-		
-		// viewMap Event Listener
-		viewMap.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String[] opt = {city1S, city2S, city3S, city4S, city5S, city6S, city7S, city8S, city9S, city10S};
-				MapScreen.run(opt);
-			}
+		Platform.runLater(() -> {
+		    WebView webView = new WebView();
+			jfxPanel.setScene(new Scene(webView));
+		    webView.getEngine().load("https://www.google.com/maps/place/" + place);
 		});
 		
 		
 		
-		//button to save results 
-		JButton save = new JButton ("Save Results");
-		save.setFont(new Font("Verdana", Font.BOLD, 40));
-		save.setBackground(new Color(182,239,225));
-		save.setBounds(1050, 750, 400, 75);
-		save.setBorderPainted(false);
-		results.add(save);
+		JFrame bframe = new JFrame("JxBrowser Google Maps");
+		JPanel jp = new JPanel(new BorderLayout());
+		bframe.setVisible(false);                                                             
+		//Browser browser = new Browser();
+		//BrowserView view = new BrowserView(browser);
+		bframe.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		//view.setSize(1500, 1080);
+		jp.add(jfxPanel, BorderLayout.CENTER);
+		bframe.add(jp);
+		bframe.setVisible(true);
+		bframe.setBounds(-8, 0, 1940, 1080);
+		//browser.loadURL("https://www.google.com/maps/place/" + place);
+
 		
-		//action listener for saving results
-		save.addActionListener(new ActionListener() {
+		// JFrame 2 Format
+		JPanel jf2 = new JPanel();
+		jf2.setLayout(new GridLayout(10, 0));
+		jf2.setSize(0, 200);
+		jf2.setVisible(true);
+		
+		
+		bframe.add(jf2, BorderLayout.LINE_END);
+		bframe.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// Call options
+		options(opt, bframe, jf2);
+	}
+
+	public static void options(String[] options, JFrame bframe, JPanel jf2) {
+		int count = 0;
+		int count2 = 0;
+		int count3 = 0;
+		int count4 = 0;
+		int count5 = 0;
+		int count6 = 0;
+		int count7 = 0;
+		int count8 = 0;
+		int count9 = 0;
+		int count10 = 0;
+		boolean test = true;
+		boolean test2 = true;
+		boolean test3 = true;
+		boolean test4 = true;
+		boolean test5 = true;
+		boolean test6 = true;
+		boolean test7 = true;
+		boolean test8 = true;
+		boolean test9 = true;
+		boolean test10 = true;
+		while (test && count <= options[0].length()) {
+			if (options[0].substring(count, count + 1).equals("+")) {
+				test = false;
+			} else {
+				count++;
+			}
+		}
+		while (test2 && count2 < options[1].length()) {
+			if (options[1].substring(count2, count2 + 1).equals("+")) {
+				test2 = false;
+			} else {
+				count2++;
+			}
+		}
+		while (test3 && count3 < options[2].length()) {
+			if (options[2].substring(count3, count3 + 1).equals("+")) {
+				test3 = false;
+			} else {
+				count3++;
+			}
+		}
+		while (test4 && count4 < options[3].length()) {
+			if (options[3].substring(count4, count4 + 1).equals("+")) {
+				test4 = false;
+			} else {
+				count4++;
+			}
+		}
+		while (test5 && count5 < options[4].length()) {
+			if (options[4].substring(count5, count5 + 1).equals("+")) {
+				test5 = false;
+			} else {
+				count5++;
+			}
+		}
+		while (test6 && count6 < options[5].length()) {
+			if (options[5].substring(count6, count6 + 1).equals("+")) {
+				test6 = false;
+			} else {
+				count6++;
+			}
+		}
+		
+		while (test7 && count7 < options[6].length()) {
+			if (options[6].substring(count7, count7 + 1).equals("+")) {
+				test7 = false;
+			} else {
+				count7++;
+			}
+		}
+		while (test8 && count8 < options[7].length()) {
+			if (options[7].substring(count8, count8 + 1).equals("+")) {
+				test8 = false;
+			} else {
+				count8++;
+			}
+		}
+		while (test9 && count9 < options[8].length()) {
+			if (options[8].substring(count9, count9 + 1).equals("+")) {
+				test9 = false;
+			} else {
+				count9++;
+			}
+		}
+		while (test10 && count10 < options[9].length()) {
+			if (options[9].substring(count10, count10 + 1).equals("+")) {
+				test10 = false;
+			} else {
+				count10++;
+			}
+		}
+		
+		
+		
+		JButton o1 = new JButton(options[0].substring(0, count));
+		o1.setFont(font);
+		o1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showConfirmDialog(null, "Your results have been saved.", "Saved",  JOptionPane.OK_CANCEL_OPTION,  JOptionPane.INFORMATION_MESSAGE);
+				place = options[0];
+				bframe.setVisible(false);
+				run(options);
 			}
 		});
-		
-		//button to go back to home screen
-		JButton home = new JButton ("Back to Home");
-		home.setFont(new Font("Verdana", Font.BOLD, 40));
-		home.setBackground(new Color(182,239,225));
-		home.setBounds(800, 850, 400, 75);
-		home.setBorderPainted(false);
-		results.add(home);
-		
-		//action listener for back to home screen button
-		home.addActionListener(new ActionListener() {
+		JButton o2 = new JButton(options[1].substring(0, count2));
+		o2.setFont(font);
+		o2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				results.setVisible(false);
-				HomeScreen.run();
+				place = options[1];
+				bframe.setVisible(false);
+				run(options);
 			}
 		});
-		
-		
-		results.setVisible(true);
+		JButton o3 = new JButton(options[2].substring(0, count3));
+		o3.setFont(font);
+		o3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[2];
+				bframe.setVisible(false);;
+				run(options);
+			}
+		});
+		JButton o4 = new JButton(options[3].substring(0, count4));
+		o4.setFont(font);
+		o4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[3];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o5 = new JButton(options[4].substring(0, count5));
+		o5.setFont(font);
+		o5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[4];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o6 = new JButton(options[5].substring(0, count6));
+		o6.setFont(font);
+		o6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[5];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o7 = new JButton(options[6].substring(0, count7));
+		o7.setFont(font);
+		o7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[6];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o8 = new JButton(options[7].substring(0, count8));
+		o8.setFont(font);
+		o8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[7];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o9 = new JButton(options[8].substring(0, count9));
+		o9.setFont(font);
+		o9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[8];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		JButton o10 = new JButton(options[9].substring(0, count10));
+		o10.setFont(font);
+		o10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				place = options[9];
+				bframe.setVisible(false);
+				run(options);
+			}
+		});
+		jf2.add(o1);
+		jf2.add(o2);
+		jf2.add(o3);
+		jf2.add(o4);
+		jf2.add(o5);
+		jf2.add(o6);
+		jf2.add(o7);
+		jf2.add(o8);
+		jf2.add(o9);
+		jf2.add(o10);
 	}
 }
